42893bc52e93199fe964f46c2863ddfa30b9e3ae
Mateus Leonardi <mateus.leonardi@resultadosdigitais.com.br>
2020-07-10 13:15:46 -0300

    Translating a few texts and cleaning up some unused code

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace TodoApi
 {
@@ -14,22 +8,15 @@ namespace TodoApi
 	{
 		public static void Main(string[] args)
 		{
-			// CreateHostBuilder(args).Build().Run();
 			var host = new WebHostBuilder()
 				.UseKestrel()
-				.UseUrls("http://*:5000") //Add this line
+				// This will allow you to access your API outside Docker container
+				.UseUrls("http://*:5000")
 				.UseContentRoot(Directory.GetCurrentDirectory())
 				.UseStartup<Startup>()
 				.UseIISIntegration()
 				.Build();
 			host.Run();
 		}
-
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Sample project - First step in docker with aspnet core
+# Sample project - First step in docker with aspnet core
+
+Simple project to show how to build an API with ASP .NET Core and run it inside a docker.
